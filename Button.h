f5ca252b17448819ef4a699f11c83b6fbf2a33ec
Is A <ismailsalama526@gmail.com>
2022-05-12 17:55:38 +0200

    Modified Button driver

--- Button.h
+++ Button.h
@@ -1,4 +1,7 @@
 #include "DigitalIO.h"
+
 void button_init(char portName, unsigned char pinNumber);
+
 unsigned char button_read(char portName, unsigned char pinNumber);
+
 void button_init_PUR(char portName, unsigned char pinNumber);
